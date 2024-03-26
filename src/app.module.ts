5f8ada51a66178cc6a315e61a21d66e618655a68
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-03-26 12:27:19 +0400

    make config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { TokenModule } from './token/token.module';
 import { UserModule } from './user/user.module';
@@ -6,6 +8,22 @@ import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [TokenModule, UserModule, AuthModule, DashboardModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: configService.get<string>('DB_NAME'),
+      }),
+    }),
+    TokenModule,
+    UserModule,
+    AuthModule,
+    DashboardModule,
+  ],
 })
 export class AppModule {}
