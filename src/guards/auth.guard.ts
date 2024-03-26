0d65953a8308328e68d1a48f613f98990d7f15a1
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-03-26 12:57:48 +0400

    complete guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,8 +1,24 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly tokenService: TokenService) {}
+
   canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest();
+    const token = this.tokenService.extractFromHeaders(req);
+
+    if (!token) throw new UnauthorizedException();
+
+    this.tokenService.verify(token);
+
     return true;
   }
 }
