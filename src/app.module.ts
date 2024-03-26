bf2b71ca99deef98916bdc8f93fe249f326ec652
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-03-26 12:18:49 +0400

    generate modules, controllers, services, guards, schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { TokenModule } from './token/token.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
 @Module({
-  imports: [],
+  imports: [TokenModule, UserModule, AuthModule, DashboardModule],
 })
 export class AppModule {}
